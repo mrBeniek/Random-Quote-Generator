f028965a6923ce6fdac228fc16f9e22a5a4bd80e
mrBeniek <b3niek@gmail.com>
2019-12-18 19:20:19 +0100

    NewQuote - changed component to functional

--- src/Components/NewQuote.js
+++ src/Components/NewQuote.js
@@ -1,10 +1,10 @@
 import React from 'react';
-export class NewQuote extends React.Component {
-  render() {
-    const QUOTE_BOX_ON = <div id="new-quote" onClick={this.props.handleClick}>NEW QUOTE</div>;
+export const NewQuote = (props, done) => {
+  
+    const QUOTE_BOX_ON = <div id="new-quote" onClick={props.handleClick}>NEW QUOTE</div>;
     const QUOTE_BOX_OFF = <div id="new-quote" style={{ color: "#E53935", cursor: "not-allowed", opacity: 0.5 }}>NEW QUOTE</div>;
     return (<React.Fragment>
-      {this.props.done ? QUOTE_BOX_ON : QUOTE_BOX_OFF}
+      {done ? QUOTE_BOX_ON : QUOTE_BOX_OFF}
     </React.Fragment>);
-  }
+  
 }
