c8e80515272e5793b19d43f68ac6b775d5c4abbc
mrBeniek <b3niek@gmail.com>
2019-12-18 19:16:31 +0100

    QuoteBox - changed component to functional

--- src/Components/QuoteBox.js
+++ src/Components/QuoteBox.js
@@ -1,28 +1,28 @@
 import React from 'react';
-export class QuoteBox extends React.Component {
-  render() {
-    const STATIC_TEXT = <h1 id="text" style={this.props.textChange}>"{this.props.res}"</h1>;
-    const ANIM_TEXT = <h1 id="text-anim" style={this.props.textChange}>"{this.props.res}"</h1>;
-    const HIDDEN_AUTHOR = <div id="author-hidden">- {this.props.author}</div>;
-    const ANIM_AUTHOR = <div id="author" style={this.props.textChange}>
-      - {this.props.author}
+export const QuoteBox = (props) => {
+  
+    const STATIC_TEXT = <h1 id="text" style={props.textChange}>"{props.res}"</h1>;
+    const ANIM_TEXT = <h1 id="text-anim" style={props.textChange}>"{props.res}"</h1>;
+    const HIDDEN_AUTHOR = <div id="author-hidden">- {props.author}</div>;
+    const ANIM_AUTHOR = <div id="author" style={props.textChange}>
+      - {props.author}
     </div>;
-    const STATIC_BOX1 = <div className="box-1" style={this.props.borderChange}></div>;
-    const ANIM_BOX1 = <div className="box-1-anim" style={this.props.borderChange}></div>;
-    const STATIC_BOX2 = <div className="box-2" style={this.props.borderChange}></div>;
-    const ANIM_BOX2 = <div className="box-2-anim" style={this.props.borderChange}></div>;
+    const STATIC_BOX1 = <div className="box-1" style={props.borderChange}></div>;
+    const ANIM_BOX1 = <div className="box-1-anim" style={props.borderChange}></div>;
+    const STATIC_BOX2 = <div className="box-2" style={props.borderChange}></div>;
+    const ANIM_BOX2 = <div className="box-2-anim" style={props.borderChange}></div>;
     return (<div id="quote-box">
 
       <div id="text-bg">
         <div id="text-container">
-          {this.props.animationStatus == "off" ? STATIC_TEXT : ANIM_TEXT}
-          {this.props.done ? ANIM_AUTHOR : HIDDEN_AUTHOR}
+          {props.animationStatus == "off" ? STATIC_TEXT : ANIM_TEXT}
+          {props.done ? ANIM_AUTHOR : HIDDEN_AUTHOR}
         </div>
-        {this.props.animationStatus == "off" ? STATIC_BOX1 : ANIM_BOX1}
-        {this.props.animationStatus == "off" ? STATIC_BOX2 : ANIM_BOX2}
+        {props.animationStatus == "off" ? STATIC_BOX1 : ANIM_BOX1}
+        {props.animationStatus == "off" ? STATIC_BOX2 : ANIM_BOX2}
       </div>
 
 
     </div>);
-  }
+  
 }
